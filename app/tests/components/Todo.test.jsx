dbd5be081ab084c5145e57aa2154e3d10bedeb59
Murali Manjunatha <Murali.Manjunatha@neudesic.com>
2016-07-04 02:56:00 +0530

    Added test to toggle

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -9,5 +9,22 @@ var Todo = require("Todo");
 describe("Todo", () => {
     it("should exits", () => {
         expect(Todo).toExist();
-    })
+    });
+
+    it("should call parent method when click todo component", () => {
+        var todos = {
+            id: 11,
+            value: "Test",
+            completed: true
+        };
+        var spy = expect.createSpy();
+        var todo = TestUtils.renderIntoDocument(<Todo {...todos} onToggle={spy}/>);
+
+        var $el = $(ReactDom.findDOMNode(todo));
+
+        TestUtils.Simulate.click($el[0]);
+
+        expect(spy).toHaveBeenCalledWith(11);
+
+    });
 })
